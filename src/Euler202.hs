39bfa6b5a8a9c66235e0397c18d984e3a59d4b60
Hackle Wayne <hacklew@hotmail.com>
2017-05-07 15:30:30 +1200

    Use a better factorizer

--- src/Euler202.hs
+++ src/Euler202.hs
@@ -10,9 +10,20 @@ bounces hits =
 isValidPoint :: Integer -> Integer -> Bool
 isValidPoint x y = y `mod` 2 == x `mod` 2
 
+primesFrom :: Integer -> [Integer] -> [Integer]
+primesFrom n xs = 
+    if any (\x -> n `mod` x == 0) xs
+        then primesFrom (n + 2) xs
+        else n:(primesFrom (n + 2) (n:xs))
+
+primes :: [Integer]
+primes = 2:(primesFrom 3 [2])
+
 factorize :: Integer -> [Integer]
-factorize n = filter (\x -> n `mod` x == 0) [2..hlf]
-    where hlf = max 2 (n `div` 2)
+factorize n = n : (concatMap byP (takeWhile (< sqroot) primes))
+    where 
+        sqroot = floor $ sqrt (fromInteger n)
+        byP p = if n `mod` p == 0 then [ p, (n `div` p) ] else []
 
 noLeak :: Integer -> Integer -> Bool
 noLeak a b = 
